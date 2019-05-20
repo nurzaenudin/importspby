77dc63c55cea02a5b32c2099747ea9f3eff05426
Nur Zaenudin <nurzaenudin@kemenkeu.go.id>
2019-05-20 12:15:50 +0700

    pertama

---
+++ src/main/java/com/nurzaenudin/importspby/ImportspbyApplication.java
@@ -0,0 +1,13 @@
+package com.nurzaenudin.importspby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportspbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportspbyApplication.class, args);
+	}
+
+}
